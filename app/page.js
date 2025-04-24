61f2580a0d7348eb4d7c5afc4d684ca7221f4f81
harshitrue <harshitrue@gmail.com>
2025-04-24 13:12:02 +0530

    Final Commit

--- app/page.js
+++ app/page.js
@@ -1,7 +1,4 @@
 "use client"
-import { Button } from "@/components/ui/button";
-import { UserButton } from "@stackframe/stack";
-import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
